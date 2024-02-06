0c54bae46becf741500625d701fa5643d557bde3
Ollie-Judge <ollie.judge@hotmail.com>
2024-02-06 12:14:58 +0000

    center aligned the buttons

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,14 +5,22 @@ import firstImg from "../assets/img1.jpg";
 
 function Gallery() {
   return (
-    <div>
-      <button id="left" onClick={() => console.log("left Click")}>
+    <div id="galleryContainer">
+      <button
+        id="left"
+        onClick={() => console.log("left Click")}
+        className="button"
+      >
         <FontAwesomeIcon icon={faArrowLeft} />
       </button>
 
       <img src={firstImg} alt="firstImg" id="firstImg" />
 
-      <button id="right" onClick={() => console.log("right Click")}>
+      <button
+        id="right"
+        onClick={() => console.log("right Click")}
+        className="button"
+      >
         <FontAwesomeIcon icon={faArrowRight} />
       </button>
     </div>
