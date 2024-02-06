b333c957e5fadd0dd796dc1a1c582647c525f62a
Ollie-Judge <ollie.judge@hotmail.com>
2024-02-06 11:44:40 +0000

    corrected errors to do with numbersin id and src

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,7 @@
 import "../style/gallery.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import firstImg from "../assets/img1.jpg";
 
 function Gallery() {
   return (
@@ -8,6 +9,8 @@ function Gallery() {
       <button id="left" onClick={() => console.log("left Click")}>
         <FontAwesomeIcon icon={faArrowLeft} />
       </button>
+
+      <img src={firstImg} alt="firstImg" id="firstImg" />
 
       <button id="right" onClick={() => console.log("right Click")}>
         <FontAwesomeIcon icon={faArrowRight} />
